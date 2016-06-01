0c785bfb274bda88a4cf824a081279a00335e8e0
tachucj <lacontealejandro@gmail.com>
2016-06-01 01:43:31 -0300

    Implementacion servicio incompleta

--- src/main/java/model/ServicioConsultaBancoImpl.java
+++ src/main/java/model/ServicioConsultaBancoImpl.java
@@ -1,27 +1,40 @@
 package model;
 
 import java.util.List;
+import java.util.Properties;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.glassfish.jersey.client.ClientConfig;
+import org.json.JSONObject;
+
+import util.PropertiesFactory;
 
 public class ServicioConsultaBancoImpl implements ServicioConsultaBanco {
 
     @Override
     public List<SucursalBanco> getBancosExternos() {
+        Properties properties = PropertiesFactory.getProperties();
+        ClientConfig config = new ClientConfig();
+        Client client = ClientBuilder.newClient(config);
+        WebTarget webTarget = client.target(properties.getProperty("url.servicio.bancos"));//.queryParam("idTarjeta", nroTarjeta);
+        Invocation.Builder invocationBuilder = webTarget.path("banks").request(MediaType.APPLICATION_JSON);
+        Response response = invocationBuilder.get(Response.class);
+        String jsonResponse=null;
+        if (response.getStatus() == 200) {
+            jsonResponse = invocationBuilder.accept(MediaType.APPLICATION_JSON).get(String.class);
+            JSONObject jsonObject = new JSONObject(jsonResponse);
+            System.out.println("Imprimiendo JSONObject: ");
+            System.out.println(jsonObject);
+        }
+        System.out.println(jsonResponse);
         return null;
-//        Client client = ClientBuilder.newClient();
-//        WebResource webResource = client.resource("http://api.grosomercado.com/debitarJSON").queryParam("idTarjeta",
-//                nroTarjeta);
-//        ClientResponse response = webResource.accept("application/json").get(ClientResponse.class);
-//        if (response.getStatus() == 200) {
-//            String jsonString = response.getEntity(String.class);
-//            JSONObject jsonObject = new JSONObject(jsonString);
-//            JSONObject miRespuesta = (JSONObject) jsonObject.get("respuesta");
-//            return Double.valueOf(miRespuesta.getString("saldo"));
-//        } else {
-//            throw new ServiceMercadoException("Error HTTP : " + response.getStatus());
-//        }
     }
 
 }
