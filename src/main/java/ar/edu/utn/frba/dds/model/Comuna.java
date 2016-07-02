cc06555f0d36c15eb6bf5422d70ca048caed1f12
Derek Baker <derekbaker90@gmail.com>
2016-07-01 22:42:51 -0300

    Ignorado el atributo 'superficie' para el JSON
    * El JSON devolvia tambien como parte de la comuna el atributo superficie, el cual es del tipo Polygon y traia todos sus objetos, generando una lista
    infinita

--- src/main/java/ar/edu/utn/frba/dds/model/Comuna.java
+++ src/main/java/ar/edu/utn/frba/dds/model/Comuna.java
@@ -2,18 +2,22 @@ package ar.edu.utn.frba.dds.model;
 
 import java.awt.Polygon;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties({ "superficie" })
 public class Comuna {
+
     private Polygon superficie;
-    
-    public void setSuperficie(Polygon superficie){
-        this.superficie=superficie;
+
+    public void setSuperficie(Polygon superficie) {
+        this.superficie = superficie;
     }
-    
-    public Polygon getSuperficie(){
+
+    public Polygon getSuperficie() {
         return superficie;
     }
-    
-    public boolean incluyeGeolocalizacion(Geolocalizacion geolocalizacion){
+
+    public boolean incluyeGeolocalizacion(Geolocalizacion geolocalizacion) {
         return superficie.contains(geolocalizacion.getLatitud(), geolocalizacion.getLongitud());
     }
 }
