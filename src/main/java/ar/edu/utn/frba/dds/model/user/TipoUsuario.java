0c9f0c51f50b4bbc62130290ed23272a2a0ce6d3
Alejandro Laconte <lacontealejandro@gmail.com>
2016-09-04 13:56:39 -0300

    Inicio Perfiles de usuario

---
+++ src/main/java/ar/edu/utn/frba/dds/model/user/TipoUsuario.java
@@ -0,0 +1,6 @@
+package ar.edu.utn.frba.dds.model.user;
+
+
+public abstract class TipoUsuario {
+    
+}
---
+++ src/main/java/ar/edu/utn/frba/dds/model/user/Usuario.java
@@ -0,0 +1,7 @@
+package ar.edu.utn.frba.dds.model.user;
+
+
+public class Usuario {
+
+    private String username;
+}
