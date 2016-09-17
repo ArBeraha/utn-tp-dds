a3de8264da2c411920aba3ffe1914802b9145389
tachucj <lacontealejandro@gmail.com>
2016-09-17 16:57:14 -0300

    Cambio return del endpoint login para que devuelva lo que necesita la UI y no de más

--- src/main/java/ar/edu/utn/frba/dds/controllers/LoginController.java
+++ src/main/java/ar/edu/utn/frba/dds/controllers/LoginController.java
@@ -2,6 +2,9 @@ package ar.edu.utn.frba.dds.controllers;
 
 import java.util.List;
 
+import org.json.JSONObject;
+import org.json.JSONStringer;
+import org.json.JSONWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.JsonNode;
 
 import ar.edu.utn.frba.dds.model.exceptions.LoginException;
 import ar.edu.utn.frba.dds.model.poi.PuntoDeInteres;
@@ -30,7 +35,11 @@ public class LoginController {
     public ResponseEntity<?> login(@RequestParam("user") String user, @RequestParam("pass") String pass) {
         try {
             Usuario usuario = appService.loginUser(user, pass);
-            return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
+            JSONObject jsonUsuario = new JSONObject();
+            jsonUsuario.accumulate("id", usuario.getId());
+            jsonUsuario.accumulate("username", usuario.getUsername());
+            jsonUsuario.accumulate("tipo", usuario.getTipoUsuario().getNombreTipoUsuario());
+            return new ResponseEntity<String>(jsonUsuario.toString(), HttpStatus.OK);
         } catch (LoginException e) {
             e.printStackTrace();
             return new ResponseEntity<String>("{Login incorrecto}", HttpStatus.BAD_REQUEST);
