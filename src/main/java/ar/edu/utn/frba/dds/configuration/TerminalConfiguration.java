c6233ec867f15bbbc8c9cc4f8936703a601aea61
tachucj <lacontealejandro@gmail.com>
2016-10-28 00:15:04 -0300

    Agregado JodaMapper en TerminalConfiguration para manejo de LocalTime y LocalDate en el serializado y deserializado de json al front

--- src/main/java/ar/edu/utn/frba/dds/configuration/TerminalConfiguration.java
+++ src/main/java/ar/edu/utn/frba/dds/configuration/TerminalConfiguration.java
@@ -1,10 +1,18 @@
 package ar.edu.utn.frba.dds.configuration;
 
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.joda.JodaMapper;
 
 @Configuration
 @EnableWebMvc
@@ -18,4 +26,17 @@ public class TerminalConfiguration extends WebMvcConfigurerAdapter {
         registry.addResourceHandler("/js/**").addResourceLocations("/js/");
         registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
     }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(converter());
+    }
+    
+    @Bean
+    public MappingJackson2HttpMessageConverter converter() {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(new JodaMapper());
+        return converter;
+    }
+    
 }
